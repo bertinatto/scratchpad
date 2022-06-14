645e48b2b0b43ff611d8b8a30c9b16d713afb3f1
Fabio Bertinatto <fbertina@redhat.com>
2022-06-14 10:37:52 -0300

    Import

---
+++ env.sh
@@ -0,0 +1,10 @@
+export DRIVER_IMAGE=quay.io/openshift/origin-gcp-pd-csi-driver:latest
+# export DRIVER_IMAGE=quay.io/openshift/origin-gcp-pd-csi-driver:latest
+export PROVISIONER_IMAGE=quay.io/openshift/origin-csi-external-provisioner:latest
+export ATTACHER_IMAGE=quay.io/openshift/origin-csi-external-attacher:latest
+export RESIZER_IMAGE=quay.io/openshift/origin-csi-external-resizer:latest
+export SNAPSHOTTER_IMAGE=quay.io/openshift/origin-csi-external-snapshotter:latest
+export NODE_DRIVER_REGISTRAR_IMAGE=quay.io/openshift/origin-csi-node-driver-registrar:latest
+export LIVENESS_PROBE_IMAGE=quay.io/openshift/origin-csi-livenessprobe:latest
+export KUBE_RBAC_PROXY_IMAGE=quay.io/openshift/origin-kube-rbac-proxy:latest
+
