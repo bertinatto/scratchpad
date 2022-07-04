c501cdb4c2fc79cf9629e8255c4bf2457fd439ea
Fabio Bertinatto <fbertina@redhat.com>
2022-07-04 17:22:51 -0300

    Initial SC controller

--- pkg/operator/starter.go
+++ pkg/operator/starter.go
@@ -3,12 +3,19 @@ package operator
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
+	gcfg "gopkg.in/gcfg.v1"
+
+	storagev1 "k8s.io/api/storage/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/dynamic"
 	kubeclient "k8s.io/client-go/kubernetes"
+	corev1listers "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/klog/v2"
+	"k8s.io/legacy-cloud-providers/gce"
 
 	opv1 "github.com/openshift/api/operator/v1"
 	configclient "github.com/openshift/client-go/config/clientset/versioned"
@@ -19,23 +26,27 @@ import (
 	"github.com/openshift/library-go/pkg/operator/csi/csicontrollerset"
 	"github.com/openshift/library-go/pkg/operator/csi/csidrivercontrollerservicecontroller"
 	"github.com/openshift/library-go/pkg/operator/csi/csidrivernodeservicecontroller"
+	"github.com/openshift/library-go/pkg/operator/csi/csistorageclasscontroller"
 	goc "github.com/openshift/library-go/pkg/operator/genericoperatorclient"
 	"github.com/openshift/library-go/pkg/operator/v1helpers"
 )
 
 const (
 	// Operand and operator run in the same namespace
-	defaultNamespace   = "openshift-cluster-csi-drivers"
-	operatorName       = "gcp-filestore-csi-driver-operator"
-	operandName        = "gcp-filestore-csi-driver"
-	secretName         = "gcp-filestore-cloud-credentials"
-	trustedCAConfigMap = "gcp-filestore-csi-driver-trusted-ca-bundle"
+	defaultNamespace     = "openshift-cluster-csi-drivers"
+	operatorName         = "gcp-filestore-csi-driver-operator"
+	operandName          = "gcp-filestore-csi-driver"
+	secretName           = "gcp-filestore-cloud-credentials"
+	trustedCAConfigMap   = "gcp-filestore-csi-driver-trusted-ca-bundle"
+	cloudConfigNamespace = "openshift-config"
+	cloudConfigName      = "cloud-provider-config"
+	cloudConfigKey       = "config"
 )
 
 func RunOperator(ctx context.Context, controllerConfig *controllercmd.ControllerContext) error {
 	// Create core clientset and informers
 	kubeClient := kubeclient.NewForConfigOrDie(rest.AddUserAgent(controllerConfig.KubeConfig, operatorName))
-	kubeInformersForNamespaces := v1helpers.NewKubeInformersForNamespaces(kubeClient, defaultNamespace, "")
+	kubeInformersForNamespaces := v1helpers.NewKubeInformersForNamespaces(kubeClient, defaultNamespace, cloudConfigNamespace, "")
 	secretInformer := kubeInformersForNamespaces.InformersFor(defaultNamespace).Core().V1().Secrets()
 	configMapInformer := kubeInformersForNamespaces.InformersFor(defaultNamespace).Core().V1().ConfigMaps()
 	nodeInformer := kubeInformersForNamespaces.InformersFor("").Core().V1().Nodes()
@@ -47,7 +58,7 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 
 	// Create GenericOperatorclient. This is used by the library-go controllers created down below
 	gvr := opv1.SchemeGroupVersion.WithResource("clustercsidrivers")
-	operatorClient, dynamicInformers, err := goc.NewClusterScopedOperatorClientWithConfigName(controllerConfig.KubeConfig, gvr, "filestore.csi.storage.gke.io") // FIXME: opv1 constant
+	operatorClient, dynamicInformers, err := goc.NewClusterScopedOperatorClientWithConfigName(controllerConfig.KubeConfig, gvr, string(opv1.GCPFilestoreCSIDriver))
 	if err != nil {
 		return err
 	}
@@ -70,7 +81,6 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 		kubeInformersForNamespaces,
 		assets.ReadFile,
 		[]string{
-			"storageclass.yaml",
 			"volumesnapshotclass.yaml",
 			"csidriver.yaml",
 			"controller_sa.yaml",
@@ -140,6 +150,13 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 		dynamicClient,
 		assets.ReadFile,
 		"servicemonitor.yaml",
+	).WithStorageClassController(
+		"GCPFilestoreStorageClassController",
+		assets.ReadFile,
+		"storageclass.yaml",
+		kubeClient,
+		kubeInformersForNamespaces.InformersFor(""),
+		// withCustomStorageClass(kubeInformersForNamespaces.ConfigMapLister().ConfigMaps(cloudConfigNamespace)),
 	)
 
 	if err != nil {
@@ -157,4 +174,40 @@ func RunOperator(ctx context.Context, controllerConfig *controllercmd.Controller
 	<-ctx.Done()
 
 	return fmt.Errorf("stopped")
+}
+
+// withCustomStorageClass gets the network name from the openshift-config/cloud-provider-config
+// ConfigMap and populate the StorageClass.Parameters.Network field with it.
+// This hook will not return any error if it fails to get the network name.
+func withCustomStorageClass(configMapLister corev1listers.ConfigMapNamespaceLister) csistorageclasscontroller.StorageClassHookFunc {
+	return func(_ *opv1.OperatorSpec, sc *storagev1.StorageClass) error {
+		cm, err := configMapLister.Get(cloudConfigName)
+		if errors.IsNotFound(err) {
+			klog.Warningf("Couldn't find the %s/%s ConfigMap: %v", cloudConfigNamespace, cloudConfigName, err)
+			return nil
+		}
+		if err != nil {
+			klog.Errorf("Failed to get the %s/%s ConfigMap: %v", cloudConfigNamespace, cloudConfigName, err)
+			return nil
+		}
+
+		data, ok := cm.Data[cloudConfigKey]
+		if !ok {
+			klog.Errorf("Failed to get the key %q in ConfigMap %s/%s", cloudConfigKey, cloudConfigNamespace, cloudConfigName)
+			return nil
+		}
+
+		cfg := &gce.ConfigFile{}
+		if err := gcfg.FatalOnly(gcfg.ReadInto(cfg, strings.NewReader(data))); err != nil {
+			klog.Errorf("Failed to parse GCE config: %v", err)
+			return nil
+		}
+
+		if cfg.Global.NetworkName != "" {
+			fmt.Printf("----------------------------------------%s\n", cfg.Global.NetworkName)
+
+			// sc.Parameters["network"] = cfg.Global.NetworkName
+		}
+		return nil
+	}
 }
